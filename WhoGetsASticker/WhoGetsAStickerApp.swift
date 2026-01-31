84f9a61b5241dfda0abfc0b22b2d1f89470c4b82
Richard Gagg <r.gagg@outlook.com>
2026-01-31 15:56:57 +1100

    Initial Commit

---
+++ WhoGetsASticker/WhoGetsAStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsAStickerApp.swift
+//  WhoGetsASticker
+//
+//  Created by Richard Gagg on 31/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct WhoGetsAStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
